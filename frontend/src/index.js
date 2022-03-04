7d23b1a0114456ab46873d0cecd32800b5786d12
Brian Vaughn <bvaughn@fb.com>
2022-03-04 11:16:05 -0500

    Removed wrapper app for clarification

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,52 @@
 import { createElement } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App';
+import {
+  createBridge,
+  createStore,
+  initialize as createDevTools,
+} from 'react-devtools-inline/frontend';
+import { io } from "socket.io-client";
 
-const root = createRoot(document.getElementById('root'));
-root.render(createElement(App));
+const HOST = 'localhost';
+const PORT = 9898;
+const UID = 123456;
+
+let root = null;
+
+const socket = io(`http://${HOST}:${PORT}`);
+socket.on("connect", () => {
+  console.log('Socket connected:', socket.id);
+
+  const wall = {
+    listen(listener) {
+      socket.on("message", (data) => {
+        console.log('socket.on("message") data:\n', data);
+
+        if (data.uid === UID) {
+          listener(data);
+        }
+      });
+    },
+    send(event, payload) {
+      const data = { event, payload, uid: UID };
+
+      console.log('socket.emit("message") data:\n', data);
+
+      socket.emit('message', data);
+    },
+  };
+
+  const bridge = createBridge(window, wall);
+  const store = createStore(bridge);
+  const DevTools = createDevTools(window, { bridge, store });
+
+  const root = createRoot(document.getElementById('root'));
+  root.render(createElement(DevTools));
+});
+socket.on("disconnect", () => {
+  console.log('Socket disconnected');
+  
+  root.unmount();
+  root = null;
+});
+
